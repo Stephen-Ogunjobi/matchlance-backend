cc3378a91150c7eb7171dcd64f3850933f493ba5
successful <stephenogunjobi400@gmail.com>
2026-03-15 14:51:47 +0100

    fix: process var

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const redisStore = new RedisStore({
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
